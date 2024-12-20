a45d8523f86f52cd61da175ca1aa4bbde61c4b00
ozamora- <ozamora-@student.42madrid.com>
2024-12-20 17:39:11 +0100

    chore: Reorganize header file includes in so_long.h

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,15 +6,21 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 20:41:00 by ozamora-          #+#    #+#             */
-/*   Updated: 2024/12/17 21:21:21 by ozamora-         ###   ########.fr       */
+/*   Updated: 2024/12/20 17:19:59 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "libft.h"
-# include "MLX42/MLX42.h"
-# include <string.h> //To use strerror
+#include "MLX42/MLX42.h"
+#include "so_long.h"
+#include "libft.h"
 
+/* Explicitly allowed functions for so_long, already included at the libraries
+# include <unistd.h> // To use system calls like write, open, read, close.
+# include <stdlib.h> // To use memory allocation (malloc, free), exit and size_t
+# include <stdio.h>  // To use perror
+# include <string.h> // To use strerror
+ */
 #endif
---
+++ src/main.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 16:37:28 by ozamora-          #+#    #+#             */
+/*   Updated: 2024/12/20 17:38:29 by ozamora-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+#define TILE_SIZE 36
+#define WIDTH TILE_SIZE*10
+#define HEIGHT TILE_SIZE*5
+
+// Exit the program as failure.
+static void ft_error(void)
+{
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+// Print the window width and height.
+static void ft_hook(void* param)
+{
+	const mlx_t* mlx = param;
+
+	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+}
+
+int32_t	main(void)
+{
+
+	// MLX allows you to define its core behaviour before startup.
+	mlx_set_setting(MLX_MAXIMIZED, true);
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+	if (!mlx)
+		ft_error();
+
+	// Create and display the image.
+	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_error();
+
+	// Even after the image is being displayed, we can still modify the buffer.
+	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
+
+	// Register a hook and pass mlx as an optional param.
+	// NOTE: Do this before calling mlx_loop!
+	mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
